6347423bc6e6d4111d3756b23c974f323eb5772c
Barnando Akbarto <barnando13@gmail.com>
2020-08-07 16:28:06 +0700

    start PaRe BoilerPlate V1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+        <div>
+            Siap
+        </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* eslint sort-imports: [2, { "ignoreMemberSort": true }] */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('app')
+);
